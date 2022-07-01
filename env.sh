5d84feeb1e9a117d14005a3bc84a564628c21eb2
Alp Akpinar <aakpinar@bu.edu>
2022-07-01 10:52:56 -0400

    Wrap env.sh into a function

--- env.sh
+++ env.sh
@@ -1,14 +1,18 @@
-# Path to BUTool must be specified from the command line
-if [ $# -eq 0 ]; then
-    echo "Please provide the path to BUTool via command line, exiting."
-    exit 1
-fi
+main () {
+    # Path to BUTool must be specified from the command line
+    if [ $# -eq 0 ]; then
+        echo "Please provide the path to BUTool via command line, exiting."
+        return
+    fi
 
-# Set the BUTOOL_PATH environment variable, to be used by the build
-export BUTOOL_PATH=${1}
+    # Set the BUTOOL_PATH environment variable, to be used by the build
+    export BUTOOL_PATH=${1}
 
-# Update the LD_LIBRARY_PATH
-DSAT_LIB_DIR=${PWD}/dsat/lib
-PLUGIN_LIB_DIR=${PWD}/lib
+    # Update the LD_LIBRARY_PATH
+    DSAT_LIB_DIR=${PWD}/dsat/lib
+    PLUGIN_LIB_DIR=${PWD}/lib
 
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${DSAT_LIB_DIR}:${PLUGIN_LIB_DIR}
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${DSAT_LIB_DIR}:${PLUGIN_LIB_DIR}
+}
+
+main
